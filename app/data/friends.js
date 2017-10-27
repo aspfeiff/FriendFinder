d6612ebbd3e894bb0f5734a03b09246d95bd5903
aspfeiff <dpfeiffer27@yahoo.com>
2017-10-27 02:35:09 -0600

    a

---
+++ app/data/friends.js
@@ -0,0 +1,21 @@
+
+var friendList = [
+  {
+    name: 'Jim Baldwin',
+    profilePic: 'https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwjbgOu6u4XXAhUkxVQKHa5oAcsQjRwIBw&url=https%3A%2F%2Fplay.google.com%2Fstore%2Fapps%2Fdetails%3Fid%3Dcom.profile.admires_stalkers_unknown&psig=AOvVaw2cZW5kS4TiQMLHvJ4ed-37&ust=1508804106701085',
+    scores: [
+      "5",
+      "3",
+      "4",
+      "4",
+      "2",
+      "5",
+      "3",
+      "4",
+      "3",
+      "3"
+    ]
+  }
+]
+
+module.exports = friendList;
